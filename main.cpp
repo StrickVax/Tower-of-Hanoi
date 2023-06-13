01614dae10baef8f30d7e38e20b57ba3fbe06f33
StrickVax <strickvax@gmail.com>
2023-06-12 23:33:15 -0700

    Set up Node

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "peg.cpp"
 
 int main()
 {
--- peg.h
+++ peg.h
@@ -1,6 +1,16 @@
 #ifndef PEG_H
 #define PEG_H
 
+class Node
+{
+    int ringSize = 0;
+    Node *next = nullptr;
+};
 
+class LinkedList
+{
+    Node *head;
+    LinkedList();
+};
 
 #endif
