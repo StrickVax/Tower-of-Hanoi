4fdbcbcef197ff824effca925f62b90cb8fe840a
StrickVax <strickvax@gmail.com>
2023-06-14 04:36:36 -0700

    Revert "Installed React Native"
    This reverts commit 603f3ed1f632bb6ac23850b80e83d4a8848fd5f2.

--- peg.h
+++ peg.h
@@ -1,7 +1,6 @@
 #ifndef PEG_H
 #define PEG_H
 
-// LINKED LIST
 class Node
 {
     int ringSize = 0;
@@ -12,18 +11,6 @@ class LinkedList
 {
     Node *head;
     LinkedList();
-
-    void pop();
-};
-
-// TOWER OF HANOI
-class Tower
-{
-    // constructors
-    Tower();
-    Tower(int numberOfRings);
-
-    void moveRing();
 };
 
 #endif
