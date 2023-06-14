9c9d830a79ceaa8c6f7b683f60cd60661028efa9
StrickVax <strickvax@gmail.com>
2023-06-14 05:16:22 -0700

    undoMove, redoMove declarations

---
+++ peg.cpp
@@ -0,0 +1,32 @@
+#include "peg.h"
+
+LinkedList::LinkedList()
+{
+    head == nullptr;
+};
+
+void LinkedList::pop(){
+
+};
+
+Tower::Tower()
+{
+    LinkedList First;
+    LinkedList Second;
+    LinkedList Third;
+};
+
+Tower::Tower(int numberOfRings = 3)
+{
+    LinkedList First;
+    LinkedList Second;
+    LinkedList Third;
+
+    for (int i = 0; i < numberOfRings; i++)
+    {
+    }
+};
+
+void Tower::moveRing(){
+
+};
--- peg.h
+++ peg.h
@@ -1,6 +1,7 @@
 #ifndef PEG_H
 #define PEG_H
 
+// LINKED LIST
 class Node
 {
     int ringSize = 0;
@@ -10,7 +11,27 @@ class Node
 class LinkedList
 {
     Node *head;
+
+public:
     LinkedList();
+
+    void pop();
+};
+
+// TOWER OF HANOI
+class Tower
+{
+    // constructors
+    Tower();
+    Tower(int numberOfRings);
+
+    LinkedList StackOfMoves;
+
+    void moveRing();
+
+    void undoMove();
+    void redoMove();
+    void displayPegs();
 };
 
 #endif
