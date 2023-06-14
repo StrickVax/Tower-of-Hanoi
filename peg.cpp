603f3ed1f632bb6ac23850b80e83d4a8848fd5f2
StrickVax <strickvax@gmail.com>
2023-06-14 00:38:08 -0700

    Installed React Native

---
+++ peg.cpp
@@ -0,0 +1,21 @@
+#include "peg.h"
+
+LinkedList::LinkedList(){
+
+};
+
+void LinkedList::pop(){
+
+};
+
+Tower::Tower(){
+
+};
+
+Tower::Tower(int numberOfRings){
+    
+};
+
+void Tower::moveRing(){
+
+};
--- peg.h
+++ peg.h
@@ -1,6 +1,7 @@
 #ifndef PEG_H
 #define PEG_H
 
+// LINKED LIST
 class Node
 {
     int ringSize = 0;
@@ -11,6 +12,18 @@ class LinkedList
 {
     Node *head;
     LinkedList();
+
+    void pop();
+};
+
+// TOWER OF HANOI
+class Tower
+{
+    // constructors
+    Tower();
+    Tower(int numberOfRings);
+
+    void moveRing();
 };
 
 #endif
